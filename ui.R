a4fce06221817d5e01590fda3f6bd01907ea361b
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2016-09-21 15:56:40 +0200

    Clean code

--- ui.R
+++ ui.R
@@ -18,20 +18,10 @@ shinyUI(fluidPage(
                h4("Model choices"),
                selectInput('estim', 'Estimator:', choices=c("Parametric", "Non-parametric")),
                numericInput('param', 'Smoothing parameter:', 1),
-               # checkboxInput("lower", "lower", FALSE),
-               # checkboxInput("stem", "Stem", FALSE),
-               # hr(),
-               # h4("Plotting"),
-               # selectInput('colorScheme', 'Color Scheme:', 
-               #             choices=c("Accent", "Dark2", "Paired", "Pastel1",
-               #                       "Pastel2", "Set1", "Set2", "Set3")),
-               # sliderInput("max", "Maximum Number of Words:", 
-               #               min = 1,  max = 300,  value = 100),
-               # hr(),
                actionButton("update","Run model")
   ),
   
-  # Show Word Cloud
+  # Show Results
   mainPanel(
     h4("Plot"),
     plotOutput("plot"),
